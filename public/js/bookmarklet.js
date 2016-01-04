802ad09f41475a5a071ac01245ae25e9194cb80d
Kyle Hardgrave <kyle@kylehardgrave.com>
2016-01-03 16:21:12 -0800

    wip

--- public/js/bookmarklet.js
+++ public/js/bookmarklet.js
@@ -1,20 +1,49 @@
-!function (window, undefined) {
-  var domain = 'http://kjh.pw/'
-  var url = window.location.href
-  var request = new XMLHttpRequest()
-  request.open('POST', domain, true)
-  request.setRequestHeader('Content-Type', 'application/json; charset=UTF-8')
-  request.send(JSON.stringify({url: url}))
-  request.onload = function () {
-    if (this.status == 201) {
-      showPopup
+/**
+ * @fileoverview Bookmarklet code for saving short links.
+ */
+;(function (window) {
+  'use strict'
+
+  var DOMAIN = 'http://kjh.pw'
+
+  /**
+   * @param {string} path
+   * @param {Object} body
+   * @param {function(Object)} onSuccess
+   * @param {function(Error)} onError
+   */
+  function post(path, body, onSuccess, onError) {
+    var request = new XMLHttpRequest()
+    request.open('POST', domain, true)
+    request.setRequestHeader('Content-Type', 'application/json; charset=UTF-8')
+    request.send(JSON.stringify(body))
+    request.onload = function () {
+      var payload = JSON.parse(this.responseText)
+      if (200 <= this.status && this.status < 400) {
+        onSuccess(payload.data)
+      } else {
+        onError(payload.error)
+      }
+    }
+    request.onerror = function () {
+      console.error("Network error", this)
+      onError()
     }
   }
-  request.onerror = function () {
-    console.error(this)
-  }
 
-  function showPopup(innerHtml) {
+  function showSuccessDialog(shortUrl) {
 
   }
-}(this)
+
+  function showError(errMsg) {
+
+  }
+
+
+  var url = window.location.href
+  post('/', {url: url}, function (data) {
+    showSuccessDialog(data.shortUrl)
+  }, function (err) {
+    showError(err.message)
+  })
+}(this));
